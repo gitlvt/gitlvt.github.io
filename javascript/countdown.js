9cd1c3c8db24a661991d327be67a48ee41733c80
gitlvt <60779667+gitlvt@users.noreply.github.com>
2021-09-09 16:45:42 +0700

    Update countdown.js

--- javascript/countdown.js
+++ javascript/countdown.js
@@ -1,1 +1,10 @@
 
+<script type="text/javascript">
+    var timeleft = 7;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdowntimer").textContent = timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);
+</script>
