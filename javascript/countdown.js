4d8bd32fc63652b40706fcf5802f889d7e60d322
gitlvt <60779667+gitlvt@users.noreply.github.com>
2021-09-12 22:42:03 +0700

    Delete javascript directory

--- javascript/countdown.js
+++
@@ -1,9 +0,0 @@
-
-var timeleft = 7;
-    var downloadTimer = setInterval(function(){
-    timeleft--;
-    document.getElementById("countdowntimer").textContent = timeleft;
-    if(timeleft <= 0)
-        clearInterval(downloadTimer);
-    },1000);
-
